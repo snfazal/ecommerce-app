7e5f5f6f472458f2310fee6249f46eaebe15c362
snfazal <snfazal@gmail.com>
2017-02-09 18:33:02 -0500

    added signup partial

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,14 @@
-console.log('js connected')
-
-var app = angular.module("myApp", []);
-
-  app.controller("MainController", ['$https', '$scope', function($https, $scope){
-
-      var controller = this;
-      this.
-
-
-
-
-
-  }]) //end MainController
+// console.log('js connected')
+//
+// var app = angular.module("myApp", []);
+//
+//   app.controller("MainController", ['$https', '$scope', function($https, $scope){
+//
+//       var controller = this;
+//       this.
+//
+//
+//
+//
+//
+//   }]) //end MainController
