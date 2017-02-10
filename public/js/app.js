5dd92141b5deabe1846bbbcbc65219c86a80bcae
snfazal <snfazal@gmail.com>
2017-02-09 21:24:01 -0500

    worked on linking frontend

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,29 @@
 console.log('js connected')
-//
-// var app = angular.module("myApp", []);
-//
-//   app.controller("MainController", ['$https', '$scope', function($https, $scope){
-//
-//       var controller = this;
-//       this.
-//
-//
-//
-//
-//
-//   }]) //end MainController
+
+angular.module('myApp');
+
+
+  .controller('MainController', MainController)
+  .controller('AuthController', AuthConroller)
+
+   function MainController($http, $scope) {
+
+      var self = this;
+
+      $scope.$on('userLoggedIn', function(event, data){
+        self.currentUser = data;
+      });
+
+    }
+
+    function AuthController($http, $state, $scope) {
+      var self = this;
+
+    function signup(userPass) {
+      $http.post('/users', userPass)
+      .then(function(response){
+        $state.go('login');
+      });
+    }
+
+    }
